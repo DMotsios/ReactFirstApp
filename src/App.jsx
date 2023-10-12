f6ae4e14dd35a022b52aa474db93efc0fbed1529
Dimitris Motsios <dmotsios@terracom.gr>
2023-10-12 13:18:33 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
+
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const value = localStorage.getItem("ITEMS");
+    console.log(value)
+    if (value == null) return []
+    return JSON.parse(value)
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ];
+    });
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id == id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    console.log(id);
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList
+        todos={todos}
+        toggleTodo={toggleTodo}
+        deleteTodo={deleteTodo}
+      />{" "}
+    </>
+  );
+}
